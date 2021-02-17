39a90901a527fd80d0cab7591f9c02c9da866f33
Sergey <sergii.pietinov@exsportia.com>
2021-02-17 21:31:54 +0200

    serik->solved: task 2

--- app.js
+++ app.js
@@ -1,46 +1,14 @@
-// import * as http from 'http';
-// import { requestHandler } from '';
-const R = require('ramda');
-const http = require('http');
+const express = require('express');
+const app = express();
 /////////////////////////////////////////////////////////////////////
-
-let users = [];
-const counter = 0;
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (R.equals(url, '/')) {
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My tasks</title></head>');
-    res.write('<body><h1>Task first!</h1><form action="/create-users" method="POST"><input type="text" name="username"/><button> Add user</button></form></body>');
-    res.write('</html>');
-
-    return res.end();
-  }
-  if (R.equals(url, '/users')) {
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Users list</title></head>');
-    users.map(el => {
-      counter++;
-      return res.write(`<li>User ${counter}:${el}</li>`)
-    });
-    res.write('</html>');
-    res.end();
-  }
-  if (R.equals(url, '/create-users') && R.equals(method, 'POST')) {
-    const body = [];
-    req.on('data', chunk => body.push(chunk));
-    req.on('end', () => {
-      const parseBody = Buffer.concat(body).toString();
-      users.push(parseBody.split('=')[1]);
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    res.end();
-  }
+app.use('/users', (req, res, next) => {
+  res.send(`<h1>Here are Users list</h1>`);
 });
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+  if (req.originalUrl === '/') return res.send(`<h1>Here is the  main page</h1>`);
+  if (req.originalUrl !== '/users') return res.send(`<h1>404 Page not exist</h1>`);
+  next();
+});
 
+app.listen(3000);
